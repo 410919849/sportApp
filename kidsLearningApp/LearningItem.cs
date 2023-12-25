44f8eb5a8a0e33632f662c010c51119edf080c6e
吳宇倫 <104067621+410919849@users.noreply.github.com>
2023-12-25 20:15:00 +0800

    Update LearningItem.cs

--- kidsLearningApp/LearningItem.cs
+++ kidsLearningApp/LearningItem.cs
@@ -12,18 +12,30 @@ namespace kidsLearningApp
         public string ImageUrl { get; set; }
     }
 
-    public class LearningData
+    public class NewsItem
     {
-        public List<LearningItem> Animals { get; set; }
-        public List<LearningItem> Fruits { get; set; }
-        public List<LearningItem> Letters { get; set; }
-        public List<LearningItem> Numbers { get; set; }
-        public List<LearningItem> Shapes { get; set; }
-        public List<LearningItem> Colors { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
 
-        public LearningData()
+    public class NewsApiResponse
+    {
+        public List<NewsItem> Articles { get; set; }
+    }
+
+    public class news
+    {
+        public List<LearningItem> 其他運動新聞 { get; set; }
+        public List<LearningItem> 肩膀訓練 { get; set; }
+        public List<LearningItem> 胸部訓練 { get; set; }
+        public List<LearningItem> 手臂訓練 { get; set; }
+        public List<LearningItem> 腿部訓練 { get; set; }
+        public List<LearningItem> 背部訓練 { get; set; }
+
+        public news()
         {
-            Letters = new List<LearningItem>()
+            胸部訓練 = new List<LearningItem>()
             {
                new LearningItem(){ImageUrl="chest.jpg"},
                new LearningItem(){Name="坐姿機械胸推\n訓練肌肉部位：肱三頭肌、大圓肌、三角肌前束、闊背肌\n\nSTEP 1 準備動作\n坐在訓練機械上，雙手握住訓練把手。\n\nSTEP 2 正式動作\n吸氣時，將雙手往胸口拉近，吐氣時，再緩緩推到初始位置。\n"},
@@ -34,7 +46,7 @@ namespace kidsLearningApp
 
             };
 
-            Numbers = new List<LearningItem>()
+            手臂訓練 = new List<LearningItem>()
             {
               new LearningItem() {ImageUrl = "arm.jpg" },
               new LearningItem() { Name = "槌式彎舉\n訓練肌肉部位：肱肌、肱二頭肌、橈肌\n\nSTEP 1 準備動作\n採站姿，雙腳打比肩寬，雙手握住啞鈴。\n\nSTEP 2 正式動作\n利用手臂力量，將啞鈴彎舉，再吐氣緩緩放下換邊。\n"},
@@ -44,7 +56,7 @@ namespace kidsLearningApp
               new LearningItem() { Name = "槓鈴手腕彎舉\n訓練肌肉部位：掌長肌、伸指肌\n\nSTEP 1 準備動作\n採坐姿，將雙腳打開跨坐在訓練椅上，背部向前彎，雙手正向握住槓鈴。\n\nSTEP 2 正式動作\n吸氣時，利用手腕將槓鈴舉起，吐氣時再緩緩放下。\n"},
             };
 
-            Colors = new List<LearningItem>()
+            背部訓練 = new List<LearningItem>()
             {
                new LearningItem() { ImageUrl = "back.jpg" },
                new LearningItem() { Name = "反手引體向上 REVERSE CHIN-UPS\nSTEP 1 準備動作：雙手與肩同寬，使用反手握法懸吊於單槓上。\nSTEP 2 正式動作：吸氣時，當下巴上台時，同時將胸廓前推，吐氣時，再緩緩下降。\n" },
@@ -55,7 +67,7 @@ namespace kidsLearningApp
 
             };
 
-            Fruits = new List<LearningItem>()
+            肩膀訓練 = new List<LearningItem>()
            {
                new LearningItem { ImageUrl = "shoulder.jpg" },
                new LearningItem { Name = "啞鈴肩推\n肩胛骨要隨動作收縮、不鎖死\n核心腹部收緊、不過度拱腰\n下背不貼死椅背，幫助穩固身體\n" },
@@ -65,14 +77,19 @@ namespace kidsLearningApp
 
            };
 
-            Shapes = new List<LearningItem>()
-            {
+            腿部訓練 = new List<LearningItem>()
+           {
                new LearningItem() { ImageUrl = "leg.jpg" },
                new LearningItem() { Name = "啞鈴單腳深蹲\n訓練肌肉部位：股四頭肌、股二頭肌\n\nSTEP 1 準備動作\n採站姿，將一隻腳往前平舉，一隻手握住啞鈴。\n\nSTEP 2 正式動作\n保持下半身穩定，將膝蓋彎曲下蹲。\n"},
                new LearningItem() { ImageUrl = "leg_a.jpg" },
                new LearningItem() { Name = "硬舉\n手抓重物，雙腳站直與肩同寬，膝蓋、肩膀向後微彎，保持脊椎挺直、腹部緊實、挺胸，緩慢地進行髖關節絞鍊（髖關節的屈曲和伸展動作）。\n當重量靠地面下降，啟動膕膀肌和臀肌力量以抗衡重物向下的拉力，盡可能降低身體，直到腿後側感受到拉扯。\n向前推動臀部回復站姿。重複你所設定的次數和組數。\n"},
                new LearningItem() { ImageUrl = "leg_b.jpg" },
                new LearningItem() { Name = "坐姿屈膝深蹲\n訓練肌肉部位：股四頭肌\n\nSTEP 1 準備動作\n雙腳站穩於訓練機上，雙腿伸直，雙手握住把手，背部靠攏。\n\nSTEP 2 正式動作\n吸氣時，將下半身往下蹲，膝蓋不超出腳尖，吐氣時，再緩緩起身。\n"},
+
+
+           };
+            其他運動新聞 = new List<LearningItem>()
+            {
 
 
             };
